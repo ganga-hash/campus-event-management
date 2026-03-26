14b6e5b1b1939e2c539a7befc50e482b74728b70
Ganga gireesh <ganga@tistcochin.edu.in>
2026-03-27 00:56:10 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,8 +4,8 @@ require('dotenv').config();
 const pool = mysql.createPool({
   host: process.env.DB_HOST || 'localhost',
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  user: process.env.DB_USER || 'guest',
+  password: process.env.DB_PASSWORD || 'guest11',
   database: process.env.DB_NAME || 'college_fest',
   waitForConnections: true,
   connectionLimit: 10,
